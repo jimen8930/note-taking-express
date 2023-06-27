a84cfd5c3cb6eab910338c6f0afbc22e36cc833f
Wayne Johnson <waynejohnson@Elizabeths-MacBook-Pro.local>
2023-06-27 00:59:28 -0400

    Added the middleware for JSON data, middleware for URL encoded data and middleware for the static file develop/public. Also, I added some  detail comments.

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
+// To import express
 const express = require('express');
+// To import the path 
 const path = require('path');
+// To import file system module
+const fs = require("fs");
+// To import unique id module to generate new id
+const uuid = require("uuid");
+
+const PORT = process.env.PORT || 3001;
 
 const app = express();
-const PORT = 3003;
+
+// Middleware for the parsing of JSON data
+app.use(express.json());
+// Middleware for the parsing of URL encoded data
+app.use(express.urlencoded({extended: true}));
+// Middleware function to serve static files
+app.use(express.static("Develop/public"));
+
+
+
 
 
 
