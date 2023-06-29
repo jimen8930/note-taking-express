af680c3b05b71dc45e88444fafc28c5d3b2e4419
Wayne Johnson <waynejohnson@Elizabeths-MacBook-Pro.local>
2023-06-28 21:15:45 -0400

    Added a read file and conditional statements to send the data.

--- server.js
+++ server.js
@@ -32,8 +32,15 @@ res.sendFile(path.join(__dirname, 'Develop/public/index.html'))
 
 // Get route request for api/notes and send backs as parsed json
   app.get('/api/notes', (req, res) => {
-    res.json(noteData);
+    fs.readFile('./Develop/db/db.json', 'utf-8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const parseData = JSON.parse(data);
+        res.status(200).json(parseData);
+      }
   });
+});
 
   app.post("/api/notes", (req, res) => {
     const { title, text } = req.body;
